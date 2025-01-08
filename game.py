aac03cf3ac1ed2f5fb621fbf97fb534610fefd08
Omkar Keni <keniomkar998@gmail.com>
2025-01-08 17:44:39 +0530

    Update game.py

--- game.py
+++ game.py
@@ -47,23 +47,3 @@ print("Goodbyee!")
 
 
 
-"""(101 , "omkar" , 10 , "yellow" , 100 , "a", "mumbai" ),
-(102 , "sai" , 9 , "red" , 55 , "c", "delhi" ),
-(103 , "vaishnavi" , 8 , "black" , 98 , "a", "banglore" ),
-(104 , "rupali" , 7 , "green" , 35 , "e", "chennai" ),
-(105 , "sanjay" , 6 , "orange" , 45 , "d", "thane" ),
-(106 , "parvati" , 5 , "grey" , 87 , "a", "mumbai" ),
-(107 , "pushpalata" , 4 , "pink" , 72 , "b", "delhi" ),
-(108 , "janardhan" , 3 , "pink" , 67 , "c", "banglore" ),
-(109 , "mayur" , 2 , "grey" , 22 , "f", "chennai" ),
-(110 , "reshma" , 1 , "red" , 77 , "b", "thane" ),
-(111 , "shiv" , 10 , "orange" , 82 , "b", "mumbai" ),
-(112 , "priyanka" , 8 , "brown" , 90 , "a", "delhi" ),
-(113 , "paresh" , 6 , "red" , 70 , "b", "banglore" ),
-(114 , "pranesh" , 10 , "blue" , 63 , "c", "chennai" ),
-(115 , "ashok" , 1 , "green" , 45 , "d", "thane" ),
-(116 , "meena" , 10 , "yellow" , 77 , "b", "mumbai" ),
-(117 , "nikhil" , 3 , "green" , 100 , "a", "delhi" ),
-(118 , "pranay" , 9 , "orange" , 16 , "f", "banglore" ),
-(119 , "om" , 5 , "yellow" , 90 , "a", "chennai" ),
-(120 , "bablu" , 4 , "blue" , 82 , "b", "thane" );"""
